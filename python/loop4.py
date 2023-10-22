45886f384e72d2de70e8ddb693c013097b2856e3
arm-td93 <86878487+armageden@users.noreply.github.com>
2023-10-22 22:13:39 +0600

    fixes

--- python/loop4.py
+++ python/loop4.py
@@ -1,18 +1,6 @@
 # home task 4 lab3
-quantity=int(input("Enter quantity: "))
-max=0
-min=0
-total=0
-avg=0
-for i in range(quantity):
-    num=int(input())
-    
-    if num > max:
-        max=num
-    if num<min:
-        min=num
-    total+=num
-avg=total/quantity
-print("Maximum=",max)
-print("Minimum=",min)
-print("Avarage is=",avg)
+N=int(input())
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print("")
--- python/loop5.py
+++ python/loop5.py
@@ -15,3 +15,22 @@ for i in range(1,num+1):
 print("Maximum",max)
 print("Minimum",min)
 print("Average is",avg)
+
+ #### or #####
+# quantity=int(input("Enter quantity: "))
+# max=0
+# min=0
+# total=0
+# avg=0
+# for i in range(quantity):
+#     num=int(input())
+    
+#     if num > max:
+#         max=num
+#     if num<min:
+#         min=num
+#     total+=num
+# avg=total/quantity
+# print("Maximum=",max)
+# print("Minimum=",min)
+# print("Avarage is=",avg)
