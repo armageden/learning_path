4d9f83817a1d04af0210883de7ecb951f6157802
arm-td93 <86878487+armageden@users.noreply.github.com>
2023-10-25 19:52:45 +0600

    mix up fixed

--- python/loop5.py
+++ python/loop5.py
@@ -1,36 +1,6 @@
-# home task 5 lab3
-num=int(input())
-max=0
-min=0
-avg=0
-sum=0
-for i in range(1,num+1):
-  inp=int(input())
-  sum+=inp
-  avg=sum/num
-  if inp>max:
-    max=inp
-  if inp<min:
-    min=inp
-print("Maximum",max)
-print("Minimum",min)
-print("Average is",avg)
-
- #### or #####
-# quantity=int(input("Enter quantity: "))
-# max=0
-# min=0
-# total=0
-# avg=0
-# for i in range(quantity):
-#     num=int(input())
-    
-#     if num > max:
-#         max=num
-#     if num<min:
-#         min=num
-#     total+=num
-# avg=total/quantity
-# print("Maximum=",max)
-# print("Minimum=",min)
-# print("Avarage is=",avg)
+# home task 4 lab3
+N=int(input())
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print("")
