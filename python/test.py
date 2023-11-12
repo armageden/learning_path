b2e9ce626d83f65d3a6cbaca959aebd420e63757
arm-td93 <86878487+armageden@users.noreply.github.com>
2023-11-12 11:06:24 +0600

    gitignore change

--- python/test.py
+++ python/test.py
@@ -1,23 +1,11 @@
-user_input = input("Enter a string: ")
-modified_string = ""
-
-# Initialize a flag to start with an uppercase character
-uppercase_next = True
-
-for char in user_input:
-    if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
-        if uppercase_next:
-            if 'a' <= char <= 'z':
-                modified_string += chr(ord(char) - ord('a') + ord('A'))
-            else:
-                modified_string += char
-        else:
-            if 'A' <= char <= 'Z':
-                modified_string += chr(ord(char) - ord('A') + ord('a'))
-            else:
-                modified_string += char
-        uppercase_next = not uppercase_next
+i = 10
+while(i >= -20):
+    if(i < 0):
+        test = " != "
+        test = str(i//2) + test + str(int(i/2))
     else:
-        modified_string += char
+        test = " == "
+        test = str(i//2) + test + str(int(i/2))
+    print(test)
+    i -= 5
 
-print("Modified string:", modified_string)
