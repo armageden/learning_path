b150ede8b7da786ec8aa4498c9bb61afaddf58be
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-02-12 20:57:17 +0600

    ...

--- python/keyGenerator.py
+++ python/keyGenerator.py
@@ -2,7 +2,15 @@
 def key_generator(*names):
     ecrpt_keys=[]
     for name in names:
-        ecrpt_key=""
-        for i in names:
-            if i:
+        ecrpt_key=chr(ord(name[0])+32)
+        for i in name[1:-1]:
+            ecrpt_key+=str(ord(i))
+        ecrpt_key+=chr(ord(name[-1])-32)    
+        ecrpt_keys.append(ecrpt_key)
+        
+    print(ecrpt_keys)
+key_list = key_generator ("Alex", "Bob", "Trudy")
+
+
+            
                 
