30dbed7ada658c9101e36b8fcbf6a8ea51e09213
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-02-28 00:10:40 +0600

    bug fix

--- python/oop/student_data.py
+++ python/oop/student_data.py
@@ -2,7 +2,34 @@
 class StudentDatabase:
     def __init__(self,name,stu_id):
         self.name=name
+        self.grades={}
         self.id=stu_id
+        
+    def calculateGPA(self, courses, semester):
+        total_gpas = 0
+        credit_p = 0
+        for course in courses:
+            course_name, gpa = course.split(':')
+            total_gpas += float(gpa)
+            credit_p += 1
+        if semester not in self.grades:
+            self.grades[semester] = {}
+        self.grades[semester][tuple(courses)] = round(total_gpas / credit_p, 2)
+    def printDetails(self):
+        print(f"Name: {self.name}")
+        print(f"ID: {self.id}")
+        for sem, course in self.grades.items():
+            print(f"Courses taken in {sem}:")
+            for names, cgpa in course.items():
+                for k in names:
+                    for l in k.split(":"):
+                        if not ("0">=l[0]<="9"):
+                            print(l)
+            
+                    
+            print(f"CGPA: {cgpa}")
+            print()        
+                
         
         
 
