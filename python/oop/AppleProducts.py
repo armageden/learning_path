cfc70159752d12a697fa3e9c4f1616d72c43d684
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-05-07 22:16:20 +0600

    jh

--- python/oop/AppleProducts.py
+++ python/oop/AppleProducts.py
@@ -14,6 +14,38 @@ class AppleProduct:
   def calculatePrice(self):
     print('Total Price:', self.base_price)
 # Write your codes here.
+class MacBookPro2020(AppleProduct):
+  def __init__(self,name,model,ram,chip,tax):
+    super().__init__(name,model,1299)
+    self.tax=tax
+    self.total=0
+    self.Total_tax=(self.base_price*tax)/100
+    self.ram=ram
+    self.chip=chip
+  def __str__(self):
+    
+    return f"{self.feature()}\nRAM:{self.ram}\nChip: {self.chip}\nCompany Details: {self.companyInfo()}"
+  def calculatePrice(self):
+    self.total=self.base_price+self.Total_tax
+    print(f"Calculating Total Price:\nBase Price: {self.base_price}\nTax: {self.tax}%\nTotal Price: {self.total}")
+  def __add__(self,obj):
+    return f"{self.total+obj.total}Dollars"
+class iPhone12(AppleProduct):
+  def __init__(self,name,model,ram,chip,tax):
+    super().__init__(name,model,799)
+    self.tax=tax
+    self.total=0
+    self.Total_tax=(self.base_price*tax)/100
+    self.ram=ram
+    self.chip=chip
+  def __str__(self):
+    
+    return f"{self.feature()}\nRAM:{self.ram}\nChip: {self.chip}\nCompany Details: {self.companyInfo()}"
+  def calculatePrice(self):
+    self.total=self.base_price+self.Total_tax
+    print(f"Calculating Total Price:\nBase Price: {self.base_price}\nTax: {self.tax}%\nTotal Price: {self.total}")
+
+
 # Do not change the following lines of code.
 
 m1 = MacBookPro2020('MacBook', 'MacBookPro2020', 8, 'M1', 10)
