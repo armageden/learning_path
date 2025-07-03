fd04776fa0cd58daedf63674b6b2ffe41c1746ca
armageden <loki4444@protonmail.com>
2025-07-03 15:20:05 +0600

    Fix input handling and ensure integer comparison in Is Sorted? implementation

--- python/codeforces/Is Sorted?.py
+++ python/codeforces/Is Sorted?.py
@@ -4,9 +4,9 @@ T=int(input())
 
 for far in range(T):
     N=int(input())
-    arr1=input().split()
+    arr1=(input().split())
     for han in range(len(arr1) - 1):
-        if arr1[han] > arr1[han + 1]:
+        if int(arr1[han]) > int(arr1[han + 1]):
             print("NO")
             break
     else:
