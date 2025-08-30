82663eea798b2cb49f61199f73c7dbb86abffc84
armageden <loki4444@protonmail.com>
2025-08-30 22:11:36 +0600

    Refactor "Where to Meet?" problem implementation to streamline Dijkstra's algorithm and improve readability

--- python/codeforces/week 7/B. Where to Meet?.py
+++ python/codeforces/week 7/B. Where to Meet?.py
@@ -1,54 +1,43 @@
 import sys
 import heapq
 
-def dijkstra(n, start, adj):
-    dist = [float('inf')] * (n + 1)
-    dist[start] = 0
-    pq = [(0, start)]
-    while pq:
-        d, u = heapq.heappop(pq)
-        if d > dist[u]:
-            continue
-        for v, w in adj[u]:
-            if dist[u] + w < dist[v]:
-                dist[v] = dist[u] + w
-                heapq.heappush(pq, (dist[v], v))
-    return dist
-
 def solve():
-    n, m, s, t = map(int, sys.stdin.readline().split())
-    
-    u_vals = list(map(int, sys.stdin.readline().split()))
-    v_vals = list(map(int, sys.stdin.readline().split()))
-    w_vals = list(map(int, sys.stdin.readline().split()))
-    
-    adj = [[] for _ in range(n + 1)]
-    rev_adj = [[] for _ in range(n + 1)]
-    for i in range(m):
-        u = u_vals[i]
-        v = v_vals[i]
-        w = w_vals[i]
-        adj[u].append((v, w))
-        rev_adj[v].append((u, w))
-    
-    dist_a = dijkstra(n, s, adj)
-    dist_b = dijkstra(n, t, rev_adj)
-    
-    min_time = float('inf')
-    meet_node = -1
-    
-    for i in range(1, n + 1):
-        if dist_a[i] != float('inf') and dist_b[i] != float('inf'):
-            meet_time = max(dist_a[i], dist_b[i])
-            if meet_time < min_time:
-                min_time = meet_time
-                meet_node = i
-            elif meet_time == min_time and i < meet_node:
-                meet_node = i
-    
-    if meet_node == -1:
+    n,m,s,t=map(int,sys.stdin.readline().split())
+    adj=[[] for _ in range(n+1)]
+    for _ in range(m):
+        u,v,w=map(int,sys.stdin.readline().split())
+        adj[u].append((v,w))
+    inf=float('inf')
+    def dijkstra(start,dists):
+        #ami ki parbo re vai?
+        for i in range(n+1):
+            dists[i]=inf
+        dists[start]=0
+        pq=[(0,start)]
+        while pq:
+            d,u=heapq.heappop(pq)
+            if d>dists[u]:
+                continue
+            for v,w in adj[u]:
+                if dists[u]+w<dists[v]:
+                    dists[v]=dists[u]+w
+                    heapq.heappush(pq,(dists[v],v))
+    ds=[0]*(n+1)
+    dt=[0]*(n+1)
+    dijkstra(s,ds)
+    #valo lage na re
+    dijkstra(t,dt)
+    mt=inf
+    mn=-1
+    for i in range(1,n+1):
+        if ds[i]!=inf and dt[i]!=inf:
+            ct=max(ds[i],dt[i])
+            if ct<mt:
+                mt=ct
+                mn=i
+    if mn==-1:
         print(-1)
     else:
-        print(min_time, meet_node)
+        print(mt,mn) #r akorbo na
 
 solve()
